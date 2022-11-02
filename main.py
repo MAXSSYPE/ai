64486b31319d21a0e8d2391be8298ec91ac18009
Maksym Dudka <dudkamv@gmail.com>
2022-11-02 23:24:30 +0200

    Lab 1_5

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ tensorflow.compat.v1.disable_v2_behavior()
 
 
 def main():
-    x = tensorflow.compat.v1.placeholder("float", [None, None])
-    function = x + 10
+    image = tensorflow.compat.v1.image.decode_png(tensorflow.compat.v1.read_file("test.png"), channels=3)
     session = tensorflow.compat.v1.Session()
-    data = [[12.3, 89.1], [123.123, -0.432], [90.1, -90.1]]
-    print(session.run(function, feed_dict={x: data}))
+    print(session.run(tensorflow.compat.v1.shape(image)))
 
 
 if __name__ == '__main__':

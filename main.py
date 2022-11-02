f8444ae583674d6565e9ef624aca2a66210fce34
Maksym Dudka <dudkamv@gmail.com>
2022-11-02 23:12:06 +0200

    Lab 1_3

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ tensorflow.compat.v1.disable_v2_behavior()
 
 
 def main():
-    x = tensorflow.compat.v1.constant(12.123, dtype=float)
-    y = tensorflow.compat.v1.Variable(x + 12.123)
-    model = tensorflow.compat.v1.global_variables_initializer()
+    x = tensorflow.compat.v1.placeholder("float", None)
+    function = x + 10
     session = tensorflow.compat.v1.Session()
-    session.run(model)
-    print(session.run(y))
+    print(session.run(function, feed_dict={x: [5.1, 10.2, 15.3, 20.4]}))
 
 
 if __name__ == '__main__':

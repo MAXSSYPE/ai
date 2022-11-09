f138148d00206d33e55c1c0ff470999af8b98fc7
Maksym Dudka <dudkamv@gmail.com>
2022-11-09 23:25:31 +0200

    Lab 1_9

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ tensorflow.compat.v1.disable_v2_behavior()
 
 
 def main():
-    x = tensorflow.compat.v1.Variable(3, dtype=tensorflow.compat.v1.float32)
-    log_x = tensorflow.compat.v1.log(x)
-    log_x_squared = tensorflow.compat.v1.square(log_x)
-    optimizer = tensorflow.compat.v1.train.GradientDescentOptimizer(0.7)
-    train = optimizer.minimize(log_x_squared)
-    init = tensorflow.compat.v1.global_variables_initializer()
+    x = tensorflow.compat.v1.placeholder(tensorflow.compat.v1.int32, [5])
+    y = tensorflow.compat.v1.placeholder(tensorflow.compat.v1.int32, [5])
+    acc, acc_op = tensorflow.compat.v1.metrics.accuracy(labels=x, predictions=y)
+    global_init = tensorflow.compat.v1.global_variables_initializer()
+    local_init = tensorflow.compat.v1.local_variables_initializer()
     session = tensorflow.compat.v1.Session()
-    session.run(init)
-    print("starting at ", "x: ", session.run(x), "log(x)^2: ", session.run(log_x_squared))
-    for step in range(10):
-        session.run(train)
-        print("step: ", step, "x: ", session.run(x), "log(x)^2: ", session.run(log_x_squared))
+    session.run(global_init)
+    session.run(local_init)
+    val = session.run([acc, acc_op], feed_dict={x: [1, 1, 0, 1, 0], y: [0, 1, 0, 0, 1]})
+    val_acc = session.run(acc)
+    print(val_acc)
 
 
 if __name__ == '__main__':

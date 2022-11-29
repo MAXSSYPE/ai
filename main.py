ef603dc3ee0a6f4eb6fe85d79029cc08fb701017
Maksym Dudka <dudkamv@gmail.com>
2022-11-29 22:25:01 +0200

    Lab 2_1

--- main.py
+++ main.py
@@ -1,43 +1,57 @@
-from __future__ import print_function
 import tensorflow
 import tensorflow_datasets
-import mnist
-import numpy
-
+import matplotlib.pyplot as plt
 
 tensorflow.compat.v1.disable_v2_behavior()
+tensorflow.compat.v1.enable_eager_execution()
 
 
 def main():
-    batch_size = 128
-    num_classes = 10
-    epochs = 2
-    x_train, y_train, x_test, y_test = mnist.fashion_mnist()
-    x_train = x_train.reshape(60000, 784)
-    x_test = x_test.reshape(10000, 784)
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
+    ds = tensorflow_datasets.load('voc/2007', split='train', shuffle_files=True)
+    for index, a in enumerate(ds):
+        plt.imshow(a['image'])
+        plt.show()
+        print(a.keys())
+        print(a['labels'].numpy())
+        if index >= 0:
+            break
 
-    x_train = (x_train - numpy.mean(x_train)) / numpy.std(x_train)
-    x_test = (x_test - numpy.mean(x_test)) / numpy.std(x_test)
+    def augment_hue(tensor):
+        print(tensor.keys())
+        return tensorflow.image.resize(tensor['image'], (62, 62)), tensor.get('label', tensor.get('labels'))
 
-    y_train = tensorflow.compat.v1.keras.utils.to_categorical(y_train, num_classes)
-    y_test = tensorflow.compat.v1.keras.utils.to_categorical(y_test, num_classes)
+    def normalize_image(image, label):
+        return image / 255.0, label
 
-    model = tensorflow.compat.v1.keras.models.Sequential()
-    model.add(tensorflow.compat.v1.keras.layers.Dense(256, activation='elu', input_shape=(784,)))
-    model.add(tensorflow.compat.v1.keras.layers.Dropout(0.4))
-    model.add(tensorflow.compat.v1.keras.layers.Dense(512, activation='relu'))
-    model.add(tensorflow.compat.v1.keras.layers.Dense(num_classes, activation='softmax'))
+    resized_dataset = ds.map(map_func=augment_hue)
+    normalized_dataset = resized_dataset.map(map_func=normalize_image)
 
-    model.summary()
+    for index, (image, label) in enumerate(resized_dataset):
+        plt.imshow(image / 255.0)
+        plt.show()
+        print(label.numpy())
+        if index >= 0:
+            break
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
-    model.save('my_model.h5')
-    score = model.evaluate(x_test, y_test, verbose=0)
-    print('Test loss: ', score[0])
-    print('Test accuracy: ', score[1])
+    model = tensorflow.keras.models.Sequential([
+        tensorflow.keras.layers.Input(shape=(62, 62, 3)),
+        tensorflow.keras.layers.Conv2D(32, kernel_size=3, activation='relu'),
+        tensorflow.keras.layers.Conv2D(32, kernel_size=3, activation='relu'),
+        tensorflow.keras.layers.GlobalMaxPooling2D(),
+        tensorflow.keras.layers.Dense(32, activation='relu'),
+        tensorflow.keras.layers.Dense(3, activation='softmax')
+    ])
+
+    # Compile and train the model for one epoch... It's only to have something trained, not get the best score
+    model.compile(
+        loss='sparse_categorical_crossentropy',
+        optimizer=tensorflow.keras.optimizers.Adam(0.001),
+        metrics=['accuracy'],
+    )
+
+    test_dataset = tensorflow_datasets.load('beans', split='test')
+    test_dataset = test_dataset.map(map_func=augment_hue).map(map_func=normalize_image).batch(8)
+    print(model.evaluate(test_dataset))
 
 
 if __name__ == '__main__':
